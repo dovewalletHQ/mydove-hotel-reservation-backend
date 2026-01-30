dadfd4aadaddb6f0ddf9c5e6891ce302c9651ce7
Lawsonredeye <omoregbeeolawson@gmail.com>
2026-01-30 16:12:38 +0100

    feat: implement suite booking functionality and add validation for existing room numbers

--- app/services/hotel_suites.py
+++ app/services/hotel_suites.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from typing import Optional
 
 from app.core.logger import setup_logger
+from app.models.booking import HotelSuiteBookingResponse, HotelSuiteBookingRequest
 from app.repositories.hotel import HotelRepository
 from app.models.hotel import HotelSuite
+from app.services.booking import BookingService
 
 
 logger = setup_logger(name="hotel_suite_service")
@@ -104,3 +107,50 @@ class HotelSuiteService:
         hotel_suite.is_available = is_available
         await hotel_suite.save()
         return hotel_suite
+
+    @staticmethod
+    async def book_suite(suite_id: str, request: HotelSuiteBookingRequest) -> Optional[HotelSuiteBookingResponse | None]:
+        if suite_id is None or request is None:
+            raise ValueError("Invalid suite id or request")
+        
+        # check if suite is available
+        try:
+            suite = await HotelRepository.get_hotel_suite_by_id(suite_id)
+            if suite is None:
+                logger.error("Suite with id '%s' not found", suite_id)
+                raise ValueError("Suite not found")
+            if not suite.is_available:
+                    logger.error("Suite with id '%s' is not available", suite_id)
+                    raise ValueError("Suite not available")
+            # booking suite for user
+            booking = await BookingService.create_booking(request.model_dump())
+            if booking is None:
+                logger.error("Error booking suite with id '%s'", suite_id)
+                raise ValueError("Error booking suite")
+            suite.is_available = False
+            await suite.save()
+            return HotelSuiteBookingResponse(
+                booking_id=str(booking.id),
+                hotel_id=booking.hotel_id,
+                suite_id=booking.suite_id,
+                suite_room_number=suite.room_number,
+                guest_name=booking.guest_name,
+                guest_phone=booking.guest_phone,
+                guest_email=booking.guest_email,
+                check_in_date=booking.check_in_date,
+                check_out_date=booking.check_out_date,
+                booking_type=booking.booking_type,
+                status=booking.status,
+                total_amount=booking.total_amount,
+                discount_amount=booking.discount_amount,
+                final_amount=booking.total_amount - booking.discount_amount,
+                number_of_guests=booking.number_of_guests,
+                special_requests=booking.special_requests,
+                booked_by_owner_id=booking.booked_by_owner_id
+            )
+        except ValueError as e:
+            logger.error("Error booking suite with id '%s': %s", suite_id[:7], e)
+            raise
+        except Exception as e:
+            logger.error("Error booking suite with id '%s': %s", suite_id[:7], e)
+            raise
